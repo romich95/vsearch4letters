9d1db2743143d9ac96c8dd2492a9adfb57195c77
Roman Mamontov <mamontov.r1995@gmail.com>
2019-02-06 12:51:47 +0300

    try/except

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -20,9 +20,16 @@ Simple example usage:
 """
 import mysql.connector
 
+class ConnectionError(Exception):
+    pass
+
+class CredentialsError(Exception):
+    pass
+
+class SQLError(Exception):
+    pass
 
 class UseDatabase:
-    
     def __init__(self, config: dict) -> None:
         """Add the database configuration parameters to the object.
 
@@ -43,9 +50,14 @@ class UseDatabase:
 
         Return the database cursor to the context manager.
         """
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:            
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.InterfaceError as err:
+            raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialsError(err)
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         """Destroy the cursor as well as the connection (after committing).
@@ -53,3 +65,7 @@ class UseDatabase:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_value)
+        elif exc_type:
+            raise exc_type(exc_value)
--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, escape, session
+from flask import copy_current_request_context
+from threading import Thread
 from vsearch import search4letters
 from checker import check_logged_in
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 
 app = Flask(__name__)
 
@@ -10,28 +12,45 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
                           'password': 'vsearchpasswd',
                           'database': 'vsearchlogDB', }
 
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out.'
+
 def log_request(req: 'flask_request', res: str) -> None:
-    """Log details of the web request and the results."""
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                (phrase, letters, ip, browser_string, results)
-                values
-                (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res, ))
+
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     """Extract the posted data; perform the search; return results."""
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Log details of the web request and the results."""
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                    (phrase, letters, ip, browser_string, results)
+                    values
+                    (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res, ))
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
+    except Exception as err:
+        print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
@@ -56,23 +75,20 @@ def view_the_log() -> 'html':
                       from log"""
             cursor.execute(_SQL)
             contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                               the_title='View Log',
+                               the_row_titles=titles,
+                               the_data=contents,)
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
     except Exception as err:
         print('Something went wrong:', str(err))
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                           the_title='View Log',
-                           the_row_titles=titles,
-                           the_data=contents,)
-
-@app.route('/login')
-def do_login() -> str:
-    session['logged_in'] = True
-    return 'You are now logged in.'
-
-@app.route('/logout')
-def do_logout() -> str:
-    session.pop('logged_in')
-    return 'You are now logged out.'
+    return 'Error'
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
 
